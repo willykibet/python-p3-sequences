176149a3d2377db36b5caa61e92586e26c476696
willykibet <willykipchumba22@gmail.com>
2023-11-27 08:55:42 +0300

    COMPLETE

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a,b=0,1
+    fib_list=[]
+    while len(fib_list) <length:
+        fib_list.append(a)
+        a, b = b, a + b
+    print(fib_list)
+
+    
